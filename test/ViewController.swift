38bb5d70214bf44229bb6a85fa2476e465ff1570
Itxaso Manzano <itxasomanzano@mac-mini-de-itxaso.local>
2017-12-01 00:16:01 +0100

    Primera subida

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -10,16 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var cajaTexto: UITextField!
+    @IBOutlet var etiqueta: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        etiqueta.text = ""
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func boton(_ sender: UIButton) {
+        let texto = cajaTexto.text!
+        let alerta :UIAlertController = UIAlertController(title: "Saludo", message: texto, preferredStyle: .alert)
+        let okAccion :UIAlertAction = UIAlertAction(title: "OK", style: .destructive, handler: nil)
+        alerta.addAction(okAccion)
+        present(alerta, animated: true, completion: nil)
+        
+        etiqueta.text = "☃️☃️ Feliz Navidad \(String(describing: texto)) ☃️☃️"
+    }
+    
+    
 }
 
